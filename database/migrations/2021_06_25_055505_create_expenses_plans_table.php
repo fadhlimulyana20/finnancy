67d2da05449a58d22abe2296874a67dd2ff53200
fadhlimulyana20 <baba.fadhli@gmail.com>
2021-06-25 14:20:39 +0700

    Menambahkan field untuk foreign pada migration

--- database/migrations/2021_06_25_055505_create_expenses_plans_table.php
+++ database/migrations/2021_06_25_055505_create_expenses_plans_table.php
@@ -15,6 +15,7 @@ class CreateExpensesPlansTable extends Migration
     {
         Schema::create('expenses_plans', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('description', 100);
             $table->string('category', 100);
