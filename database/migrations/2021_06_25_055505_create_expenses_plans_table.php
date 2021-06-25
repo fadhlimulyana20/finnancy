9d3f54e21c9dce044d6f0290d9540bb55cdc332d
adafatya <aufaditya1895@gmail.com>
2021-06-25 13:32:32 +0700

    add migration table

---
+++ database/migrations/2021_06_25_055505_create_expenses_plans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExpensesPlansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses_plans', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('description', 100);
+            $table->string('category', 100);
+            $table->bigInteger('amount');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expenses_plans');
+    }
+}
